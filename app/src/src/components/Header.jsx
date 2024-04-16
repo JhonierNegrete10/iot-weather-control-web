5881bd42b7d9a08364571bc9f147a05e8fa06a55
JhonierNegrete10 <jhoniernegretem@gmail.com>
2024-04-16 14:54:32 -0500

    add header

---
+++ app/src/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+<header style={{ background: '#272725', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+{/* aqui antes iba un logo, se puede poner o quitar */}
+</header>
+      );
+    };
+    
+export default Header;
