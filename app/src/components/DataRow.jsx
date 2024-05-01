49d6a00ca5062f20680429a3a75d71232d54ac76
JhonierNegrete10 <jhoniernegretem@gmail.com>
2024-05-01 01:40:36 -0500

    Refactor DataRow component to use Grid layout
    Simplify DataRow component by refactoring to use Grid layout for better
    responsiveness and readability. Update styling and structure accordingly.

--- app/src/components/DataRow.jsx
+++ app/src/components/DataRow.jsx
@@ -1,112 +1,53 @@
-/* eslint-disable react/prop-types */
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import OpacityIcon from "@mui/icons-material/Opacity";
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
 import StatBox from "./StatBox";
 
 const DataRow = ({ colors, data }) => {
-  const propsBoxData = {
-    gridColumn: "span 1",
+  if (data.length === 0) {
+    return null;
+  }
+  const titles = ["Temperatura", "Humedad", "Válvula"]
+  const boxStyle = {
     backgroundColor: "#fff",
     display: "flex",
     alignItems: "center",
     borderRadius: "15px",
     justifyContent: "center",
+    paddingX: "20px",
+    paddingY: "20px",
   };
-
-  if (data.length === 0) {
-    return null;
-  }
-
-  const fontSize = "12px";
-  const propsStatBoxData0 = {
-    title: "Temperatura",
-    subtitle1: data[0].subtitle1,
-    subtitle2: data[0].subtitle2,
-    value1: data[0].value1,
-    value2: data[0].value2,
-    icon: (
-      <WbSunnyIcon sx={{ color: colors.greenAccent[600], fontSize: "26px" }} />
-    ),
-    fontSize: fontSize,
-  };
-
-  const propsStatBoxData1 = {
-    title: "Humedad",
-    subtitle1: data[1].subtitle1,
-    subtitle2: data[1].subtitle2,
-    value1: data[1].value1,
-    value2: data[1].value2,
-    fontSize: fontSize,
-    icon: (
-      <OpacityIcon sx={{ color: colors.greenAccent[600], fontSize: "26px" }} />
-    ),
-  };
-
-  const propsStatBoxData2 = {
-    title: "Válvula",
-    subtitle1: data[2].subtitle1,
-    subtitle2: data[2].subtitle2,
-    fontSize: fontSize,
-    value1: data[2].value1,
-    value2: data[2].value2,
-    icon: (
-      <LocalFloristIcon
-        sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-      />
-    ),
-  };
+  
+  const iconsSx = { color: colors.greenAccent[600], fontSize: "26px" }
+  const icons = [
+    <WbSunnyIcon sx={iconsSx} />,
+    <OpacityIcon sx={iconsSx} />,
+    <LocalFloristIcon sx={iconsSx} />,
+  ];
 
   return (
     <Box
-      display="grid"
-      marginTop="20px"
-      marginBottom="20px"
-      gridTemplateColumns="repeat(3, 1fr)"
-      gridAutoRows="140px"
+      marginTop={{ xs: '10px', sm: '20px' }}
+      marginBottom={{ xs: '10px', sm: '20px' }}
       borderRadius="5px"
-      gap="10px"
     >
-      <MiddleDataBox {...propsBoxData} {...propsStatBoxData0} />
-      <MiddleDataBox {...propsBoxData} {...propsStatBoxData1} />
-      <MiddleDataBox {...propsBoxData} {...propsStatBoxData2} />
-    </Box>
-  );
-};
-
-const MiddleDataBox = ({
-  borderRadius,
-  gridColumn,
-  backgroundColor,
-  display,
-  alignItems,
-  justifyContent,
-  title,
-  icon,
-  subtitle1,
-  value1,
-  subtitle2,
-  value2,
-}) => {
-  return (
-    <Box
-      borderRadius={borderRadius}
-      gridColumn={gridColumn}
-      backgroundColor={backgroundColor}
-      display={display}
-      alignItems={alignItems}
-      justifyContent={justifyContent}
-      sx={{ paddingX: "20px" }}
-    >
-      <StatBox
-        title={title}
-        subtitle1={subtitle1}
-        value1={value1}
-        subtitle2={subtitle2}
-        value2={value2}
-        icon={icon}
-      />
+      <Grid container spacing={1}>
+        {data.map((item, index) => (
+          <Grid item xs={12} sm={4} key={index}>
+            <Box {...boxStyle}>
+              <StatBox
+                title={titles[index]}
+                subtitle1={item.subtitle1}
+                value1={item.value1}
+                subtitle2={item.subtitle2}
+                value2={item.value2}
+                icon={icons[index]}
+              />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
